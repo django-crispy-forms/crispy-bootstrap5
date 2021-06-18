e1c178894ece0d00b17c5415a2989135393c6fa6
JimiRecard <58371938+JimiRecard@users.noreply.github.com>
2021-06-18 03:32:41 -0300

    Added support for accordion flush and always open (#63)

--- crispy_bootstrap5/bootstrap5.py
+++ crispy_bootstrap5/bootstrap5.py
@@ -1,3 +1,4 @@
+from crispy_forms.bootstrap import Accordion
 from crispy_forms.layout import Field
 
 
@@ -7,3 +8,26 @@ class FloatingField(Field):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.attrs["placeholder"] = self.fields[0]
+
+
+class BS5Accordion(Accordion):
+    """
+    Bootstrap5 Accordion menu object. It wraps `AccordionGroup` objects in a
+    container. It also allows the usage of accordion-flush, introduced in bootstrap5::
+
+        BS5Accordion(
+            AccordionGroup("group name", "form_field_1", "form_field_2"),
+            AccordionGroup("another group name", "form_field"),
+            flush=True,
+            always_open=True
+        )
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.flush = kwargs.pop("flush", False)
+        self.always_open = kwargs.pop("always_open", False)
+
+        if self.always_open:
+            for accordion_group in self.fields:
+                accordion_group.always_open = True
