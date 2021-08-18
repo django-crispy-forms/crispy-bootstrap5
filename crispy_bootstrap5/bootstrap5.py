7f14ac490b6f0c28549d801a284b64f686271338
Bart van den Dool <2692194+bcvandendool@users.noreply.github.com>
2021-08-18 12:07:53 +0200

    Remove obsolete init function

--- crispy_bootstrap5/bootstrap5.py
+++ crispy_bootstrap5/bootstrap5.py
@@ -5,9 +5,6 @@ from crispy_forms.layout import Field
 class FloatingField(Field):
     template = "bootstrap5/layout/floating_field.html"
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
 
 class BS5Accordion(Accordion):
     """
